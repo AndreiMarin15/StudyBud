98144059f20a47727cb5b3f673ed258365dcc85b
yanajuson <kay_eliana_juson@dlsu.edu.ph>
2023-03-16 19:55:45 +0800

    .

---
+++ app/src/main/java/com/mobdeve/s11/hartigango/juson/marin/studybud/ReminderDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s11.hartigango.juson.marin.studybud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReminderDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.reminderdetails_screen)
+    }
+}
