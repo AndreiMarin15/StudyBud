693ec69d9a7b1d8ede1864ea5ba6d356de66f8ba
Kenji Hartigan-Go <kenji.hartigan.go@gmail.com>
2023-03-17 18:12:42 +0800

    added tasks activity

---
+++ app/src/main/java/com/mobdeve/s11/hartigango/juson/marin/studybud/tasks_screen.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s11.hartigango.juson.marin.studybud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tasks_screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks_screen)
+    }
+}
