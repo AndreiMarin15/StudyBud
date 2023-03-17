af7630d4e98502bbf0825cdd3e19135509274be5
yanajuson <kay_eliana_juson@dlsu.edu.ph>
2023-03-17 17:49:30 +0800

    new screens

---
+++ app/src/main/java/com/mobdeve/s11/hartigango/juson/marin/studybud/AddTaskActivity.kt
@@ -0,0 +1,11 @@
+package com.mobdeve.s11.hartigango.juson.marin.studybud
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.addtask_screen)
+    }
+}
