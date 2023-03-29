d5f87591ce6854b52f36e561e5b1c39b70982bf5
AndreiMarin15 <misael_marin@dlsu.edu.ph>
2023-03-30 01:01:05 +0800

    .

--- app/src/main/java/com/mobdeve/s11/hartigango/juson/marin/studybud/InfoActivity.kt
+++ app/src/main/java/com/mobdeve/s11/hartigango/juson/marin/studybud/InfoActivity.kt
@@ -4,21 +4,33 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.mobdeve.s11.hartigango.juson.marin.studybud.databinding.InfoScreenBinding
 
 class InfoActivity : AppCompatActivity(){
     private lateinit var binding: InfoScreenBinding
-
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.binding = InfoScreenBinding.inflate(layoutInflater)
         setContentView(this.binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+        val displayName = intent.getStringExtra("name")
 
         binding.confirmBtn.setOnClickListener {
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
 
         }
+        binding.welcomeMsg2.setOnClickListener {
+            auth.signOut()
+
+            startActivity(Intent(this, MainActivity:: class.java))
+        }
+
+        binding.welcomeMsg2.text = "Welcome, ${displayName}"
 
         binding.editCollege.setOnClickListener{
             binding.editCollege.setText("College of Computer Studies")
--- app/src/main/java/com/mobdeve/s11/hartigango/juson/marin/studybud/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s11/hartigango/juson/marin/studybud/MainActivity.kt
@@ -1,24 +1,89 @@
 package com.mobdeve.s11.hartigango.juson.marin.studybud
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.mobdeve.s11.hartigango.juson.marin.studybud.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
+    private lateinit var gsc: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(this.binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("136493390124-rjm4gqad82l7q78n8098cfhrs6861v2r.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+
+        gsc = GoogleSignIn.getClient(this, gso)
+        gsc.signOut()
+
         binding.btnCont.setOnClickListener(View.OnClickListener {
-        val intent = Intent(this, InfoActivity::class.java)
-        startActivity(intent)
+        //val intent = Intent(this, InfoActivity::class.java)
+        //startActivity(intent)
+
+            signInGoogle()
         })
 
+    }
 
+    private fun signInGoogle() {
+        val signInIntent = gsc.signInIntent
+        launcher.launch(signInIntent)
+
+    }
+
+    private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+        result -> if(result.resultCode == Activity.RESULT_OK) {
+                val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                handleResults(task)
+                 }
+    }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>) {
+        task.addOnCompleteListener { signInTask ->
+            if (signInTask.isSuccessful) {
+                val account = signInTask.result
+                if (account != null) {
+                    updateUI(account)
+                }
+            } else {
+                Toast.makeText(this, "Sign-in failed: ${signInTask.exception}", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener{
+            if(it.isSuccessful){
+                val intent = Intent(this, InfoActivity::class.java)
+                intent.putExtra("email", account.email)
+                intent.putExtra("name", account.displayName)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
